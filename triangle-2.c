3db2cf72360896cef113205553a8ba776874078f
Ashraf <sm.ashraf.zaman>
2020-04-17 15:11:37 +0600

    Calculate area of triangle in C programming

---
+++ triangle-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    double a,b,c,s,area;
+
+    printf("Enter 3 values : ");
+    scanf("%lf %lf %lf",&a,&b,&c);
+
+    s = (a+b+c)/2;
+    area = sqrt(s*(s-a)*(s-b)*(s-c));
+
+    printf("Area of triangle : %.2lf",area);
+
+    getch();
+    return 0;
+}
