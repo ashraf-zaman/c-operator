354ec96a3936a4ce4107ec8bcf5567ffe1d96aa0
Ashraf <sm.ashraf.zaman>
2020-04-17 15:00:37 +0600

    sum operation in C programming

---
+++ sum.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int num1, num2, sum;
+
+    printf("Enter two integer number : ");
+    scanf ("%d %d", &num1, &num2);
+
+    sum = num1+num2;
+
+    printf("The sum is = %d",sum);
+
+    getch();
+    return 0;
+}
